00013a1525ac9ce19837e2f5980d8bf1e36f1b2f
ateet101 <ateet101@gmail.com>
2018-01-29 19:39:51 -0500

    Almost There

---
+++ assets/menu/custom.js
@@ -0,0 +1,22 @@
+(function ($) {
+
+    // Menu filer
+    $("#menu-flters li a").click ( function() {
+      $("#menu-flters li a").removeClass('active');
+      $(this).addClass('active');
+
+      var selectedFilter = $(this).data("filter");
+    //  $("#menu-wrapper").fadeTo(100, 0);
+
+
+       $(".menu-restaurant").fadeOut();
+	$(".menu-default").fadeOut();
+
+      setTimeout(function() {
+        $(selectedFilter).slideDown();
+        //$("#menu-wrapper").fadeTo(300, 1);
+      }, 300);
+    });
+
+   
+})(jQuery);
